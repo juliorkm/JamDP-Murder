b63a4414570607e378bb230f894502200b85058a
juliorkm <julioramakrsnamandoju@gmail.com>
2018-03-04 13:30:37 -0300

    Score Scene
    - Cena de score;
    - Font do vinivicious;
    - Início do player select.

---
+++ Assets/Resources/Scripts/ScoreScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreScene : MonoBehaviour {
+    
+    public TextMeshProUGUI bottomScore, upperScore;
+    public Vector2 bottomStop, upperStop;
+    public Vector2 bottomEnd, upperEnd;
+
+    private int playerOneScore, playerTwoScore;
+    
+    void Start () {
+        playerOneScore = PlayerPrefs.GetInt("player1");
+        playerTwoScore = PlayerPrefs.GetInt("player2");
+        bottomScore.text = playerOneScore + " x " + playerTwoScore;
+        upperScore.text = playerTwoScore + " x " + playerOneScore;
+        StartCoroutine(ScoreAnimation());
+	}
+	
+    IEnumerator ScoreAnimation() {
+        var bottomRect = bottomScore.GetComponent<RectTransform>();
+        var upperRect = upperScore.GetComponent<RectTransform>();
+        while (bottomRect.anchoredPosition.y > bottomStop.y + 10) {
+            bottomRect.anchoredPosition =
+                Vector2.Lerp(bottomRect.anchoredPosition, bottomStop, .15f);
+            upperRect.anchoredPosition =
+                Vector2.Lerp(upperRect.anchoredPosition, upperStop, .15f);
+
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForSeconds(.8f);
+        while (bottomRect.anchoredPosition.y > bottomEnd.y + 10)
+        {
+            bottomRect.anchoredPosition =
+                Vector2.Lerp(bottomRect.anchoredPosition, bottomEnd, .4f);
+            upperRect.anchoredPosition =
+                Vector2.Lerp(upperRect.anchoredPosition, upperEnd, .4f);
+
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene("Gameplay");
+    }
+}
