a768bbf6abde41378f83a043003f2d1c59a09fba
Takehime <silviapimpaovasquez@hotmail.com>
2018-03-04 02:52:23 -0300

    geraçao da grid e spawn do player (incompleto)

---
+++ Assets/Scripts/GridTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour {
+
+	int id;
+
+	public void generateTile(int i) {
+		gameObject.name = "Tile " + i;
+		
+		id = i;
+	}
+}
