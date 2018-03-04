347a37e8555cbe7f6f4d0436ac416a784bb86947
juliorkm <julioramakrsnamandoju@gmail.com>
2018-03-04 01:07:35 -0300

    Tentatitva 1 de consertar button down
    - Reorganização de pastas;
    - Tentativa de fazer ícone acompanhar pressionamento do botão.

---
+++ Assets/Resources/Scripts/ButtonIconMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonIconMovement : MonoBehaviour {
+
+    private Button fatherButton;
+    private RectTransform rt;
+    private Vector3 nonPressedPosition;
+
+	void Start () {
+        fatherButton = GetComponentInParent<Button>();
+        rt = GetComponent<RectTransform>();
+        nonPressedPosition = new Vector3(0, 15, 0);
+        //isTopPlayer = (fatherButton.transform.rotation.eulerAngles.z > 170);
+	}
+
+    public void MoveIconDown() {
+        rt.localPosition = Vector3.zero;
+    }
+
+    public void MoveIconUp() {
+        rt.localPosition = nonPressedPosition;
+    }
+}
