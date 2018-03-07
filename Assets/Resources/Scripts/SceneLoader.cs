c6c50c2752b0b686647815fe616fc361749b67f5
juliorkm <julioramakrsnamandoju@gmail.com>
2018-03-07 20:45:24 -0300

    Ambos os jogadores devem dar Play
    - Agora clicar em Play pinta o botão da cor do seu personagem e trava a seleção de cores;
    - Se ambos os jogadores apertarem Play, começa o jogo.

--- Assets/Resources/Scripts/SceneLoader.cs
+++ Assets/Resources/Scripts/SceneLoader.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour {
+
+    private bool upperReady = false, bottomReady = false;
+    public Image upperPlay, bottomPlay;
+    public GameObject upperBlock, bottomBlock;
 
     public void ChangeScene(string s) {
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene(s);
 	}
+
+    public void SetUpperReady() {
+        upperReady = !upperReady;
+        if (upperReady)
+            upperPlay.color = ColorManager.colors[(int)ColorManager.upperPlayerColor];
+        else
+            upperPlay.color = Color.white;
+        if (upperReady && bottomReady) ChangeScene("Gameplay");
+        upperBlock.SetActive(upperReady);
+    }
+
+    public void SetBottomReady() {
+        bottomReady = !bottomReady;
+        if (bottomReady)
+            bottomPlay.color = ColorManager.colors[(int)ColorManager.bottomPlayerColor];
+        else
+            bottomPlay.color = Color.white;
+        if (upperReady && bottomReady) ChangeScene("Gameplay");
+        bottomBlock.SetActive(bottomReady);
+    }
+
 }
